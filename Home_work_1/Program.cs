edcab7ff5cf4ca2c3197269a321efa9861bbfd3c
ZubovskiyNikolay <zubriknb@gmail.com>
2023-05-24 23:54:17 +0300

    Решение Задачи 3(6)

--- Home_work_1/Program.cs
+++ Home_work_1/Program.cs
@@ -58,3 +58,15 @@
 // 4 -> да
 // -3 -> нет
 // 7 -> нет
+
+Console.WriteLine("Введите число");
+int num=Convert.ToInt32(Console.ReadLine());
+
+if (num%2==0)
+{
+  Console.WriteLine("Число четное");
+}
+else
+{
+  Console.WriteLine("Число нечетное");
+}
