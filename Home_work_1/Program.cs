67217052cb0c5ae2e1fa84aae2c7a287f646eb0f
ZubovskiyNikolay <zubriknb@gmail.com>
2023-05-24 23:02:02 +0300

    Описание Задачи 2(4), задача 1(2) закомментирована

--- Home_work_1/Program.cs
+++ Home_work_1/Program.cs
@@ -1,25 +1,32 @@
-﻿// -----------------------------Задача 1----------------------
+﻿// -----------------------------Задача 1(2)----------------------
+
 // Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
 // a = 5; b = 7 -> max = 7
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
 
-Console.WriteLine("Введите первое число");
-int num1=Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите второе число");
-int num2=Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите первое число");
+// int num1=Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число");
+// int num2=Convert.ToInt32(Console.ReadLine());
 
-if (num1==num2)
-{
-  Console.WriteLine("Числа равны");
-}
-else if (num1>num2)
-{
-  Console.WriteLine("Большее число - "+num1);
-  Console.WriteLine("Меньшее число - "+num2);
-}
-else
-{
-  Console.WriteLine("Большее число - "+num2);
-  Console.WriteLine("Меньшее число - "+num1);
-}
+// if (num1==num2)
+// {
+//   Console.WriteLine("Числа равны");
+// }
+// else if (num1>num2)
+// {
+//   Console.WriteLine("Большее число - "+num1);
+//   Console.WriteLine("Меньшее число - "+num2);
+// }
+// else
+// {
+//   Console.WriteLine("Большее число - "+num2);
+//   Console.WriteLine("Меньшее число - "+num1);
+// }
+// ----------------------------------Задача 2(4)--------------------------
+
+// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
