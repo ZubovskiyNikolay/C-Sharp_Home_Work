d9cef8a9f6ba336679cd72c2c5548526307625e6
ZubovskiyNikolay <zubriknb@gmail.com>
2023-05-24 23:46:31 +0300

    Описание Задачи 3(6), задача 2(4) закомментирована

--- Home_work_1/Program.cs
+++ Home_work_1/Program.cs
@@ -24,6 +24,7 @@
 //   Console.WriteLine("Большее число - "+num2);
 //   Console.WriteLine("Меньшее число - "+num1);
 // }
+
 // ----------------------------------Задача 2(4)--------------------------
 
 // Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
@@ -31,22 +32,29 @@
 // 44 5 78 -> 78
 // 22 3 9 -> 22
 
-Console.WriteLine("Введите первое число");
-int num1=Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите первое число");
+// int num1=Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите второе число");
-int num2=Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите второе число");
+// int num2=Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите третье число");
-int num3=Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите третье число");
+// int num3=Convert.ToInt32(Console.ReadLine());
 
-int max=num1;
-if (num2>max)
-{
-  max=num2;
-} 
-if (num3>max)
-{
-  max=num3;
-}
-Console.WriteLine("Максимальное число - "+max);
+// int max=num1;
+// if (num2>max)
+// {
+//   max=num2;
+// } 
+// if (num3>max)
+// {
+//   max=num3;
+// }
+// Console.WriteLine("Максимальное число - "+max);
+
+// -----------------------------------Задача 3(6)------------------------------
+
+// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
